d6ccc3cdc76fa1ebefaa98093be6c781287cd253
Oskar Hartbrich <hartbricho@ornl.gov>
2023-09-07 11:34:51 -0400

    standalone text parser + test data

---
+++ dt5202_parse.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python3
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import argparse
+
+import uproot
+
+
+def dt5202_text(file_name):
+        with open(file_name, 'r') as file:
+                tstamps = []
+                trgids = []
+                channels = []
+                LGs = []
+                HGs = []
+
+                channels_evt = []
+                LGs_evt = []
+                HGs_evt = []
+
+
+                for line in file:
+                        
+                        if line.startswith('//'):
+                                pass
+                        elif 'Tstamp_us' in line:
+                                pass
+                        else:
+                                tokens = line.split()
+                                if len(tokens) == 6:
+                                        if len(tstamps) > 0:
+                                                channels.append(channels_evt)
+                                                LGs.append(LGs_evt)
+                                                HGs.append(HGs_evt)
+
+                                                channels_evt = []
+                                                LGs_evt = []
+                                                HGs_evt = []
+
+                                        tstamps.append(float(tokens[0]))
+                                        trgids.append(int(tokens[1]))
+                                        channels_evt.append(int(tokens[3]))
+                                        LGs_evt.append(int(tokens[4]))
+                                        HGs_evt.append(int(tokens[5]))
+
+                                elif len(tokens) == 4:
+                                        channels_evt.append(int(tokens[1]))
+                                        LGs_evt.append(int(tokens[2]))
+                                        HGs_evt.append(int(tokens[3]))
+
+                channels.append(channels_evt)
+                LGs.append(LGs_evt)
+                HGs.append(HGs_evt)
+                                
+        return tstamps, trgids, channels, LGs, HGs
+
+       
+
+
+def flatten(tstamps, trgids, channels, LGs, HGs):
+        tstamps_flat = []
+        trgids_flat = []
+        channels_flat = []
+        LGs_flat = []
+        HGs_flat = []
+
+        for i in range(len(tstamps)):
+                for j in range(len(channels[i])):
+                        tstamps_flat.append(tstamps[i])
+                        trgids_flat.append(trgids[i])
+                        channels_flat.append(channels[i][j])
+                        LGs_flat.append(LGs[i][j])
+                        HGs_flat.append(HGs[i][j])
+
+        return np.array(tstamps_flat), np.array(trgids_flat), np.array(channels_flat), np.array(LGs_flat), np.array(HGs_flat)
+
+def save_as_numpy(tstamps, trgids, channels, LGs, HGs, file_out):
+        tstamps_flat, trgids_flat, channels_flat, LGs_flat, HGs_flat = flatten(tstamps, trgids, channels, LGs, HGs)
+
+        data = np.array([tstamps_flat, trgids_flat, channels_flat, LGs_flat, HGs_flat])
+        np.save(file_out, data)
+
+
+def save_as_root(tstamps, trgids, channels, LGs, HGs, file_out):
+    file = uproot.recreate(file_out)
+
+    file["tree"] = {"t_stamp": tstamps, 
+                    "trgid": trgids,
+                    "channel": channels,
+                    "LG": LGs,
+                    "HG": HGs}
+    file.close()
+
+def save_as_dataframe(tstamps, trgids, channels, LGs, HGs, file_out):
+        tstamps_flat, trgids_flat, channels_flat, LGs_flat, HGs_flat = flatten(tstamps, trgids, channels, LGs, HGs)
+
+        df = pd.DataFrame({'t_stamp': tstamps_flat,
+                   'trgid': trgids_flat,
+                   'channel': channels_flat,
+                   'LG': LGs_flat,
+                   'HG': HGs_flat})
+
+        df.to_pickle(file_out)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--root', default=False, action='store_true', help='output as root file')
+    parser.add_argument('-p', '--pandas', default=False, action='store_true', help='output as pickled pandas dataframe')
+    parser.add_argument('-n', '--numpy',  default=False, action='store_true', help='output as numpy file')
+    parser.add_argument('file', type=str, nargs='+', help='Input file(s)')
+    
+    args = parser.parse_args()
+
+    if not (args.root or args.pandas or args.numpy):
+        parser.error('No output format specified, add -r, -p or -n')
+    
+    if not args.file:
+        parser.error('No input file specified')
+
+    for f in args.file:
+        if not f.endswith('.txt'):
+            parser.error('Input file must be a .txt file')
+        
+    for f in args.file:
+        print(f'{f}: ', end = '')
+        tstamps, trgids, channels, LGs, HGs = dt5202_text(f)
+        if args.root:
+            file_out_root = f.replace('.txt', '.root')
+            save_as_root(tstamps, trgids, channels, LGs, HGs, file_out_root)
+            print(f'root ', end = '')    
+        if args.pandas:
+            file_out_pkl = f.replace('.txt', '.pkl')
+            save_as_dataframe(tstamps, trgids, channels, LGs, HGs, file_out_pkl)
+            print(f'pandas ', end = '')    
+        if args.numpy:
+            file_out_npy = f.replace('.txt', '.npy')
+            save_as_numpy(tstamps, trgids, channels, LGs, HGs, file_out_npy)
+            print(f'numpy ', end = '')    
+
+    print('done.')
+
+
